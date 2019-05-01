5d9eec82d9c4604a49e7dba4df0d4aea0315dde9
Becki Blumer <rblumer13@gmail.com>
2019-04-30 21:00:24 -0500

    updated styling of projects section

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 $('.learn-more').click(function() {
-    console.log('Learn more was clicked');
     $('.image-text').toggleClass('hidden');
     $('.photo-fade').toggleClass('faded');
+})
+
+$('.menu-icon').click(function(){
+
 })
