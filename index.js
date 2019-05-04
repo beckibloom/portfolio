12386656feb3997467eb1ae18e5156811d868008
Becki Blumer <rblumer13@gmail.com>
2019-05-04 10:25:50 -0500

    updated styling and js for dropdown content

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-$('.learn-more').click(function() {
+function toggleAbout() {
     $('.image-text').toggleClass('hidden');
     $('.photo-fade').toggleClass('faded');
+};
+
+$('.js-about').click(function() {
+    toggleAbout();
 })
 
-$('.menu-icon').click(function(){
+$('.learn-more').click(function() {
+    toggleAbout();
+})
 
+$('.dropdown').click(function(){
+    $('.dropdown-content').css("display", "block");
+})
+
+$(document).click(function(event) {
+    $target = $(event.target);
+    if(!$target.closest('.dropdown').length &&
+    $('.dropdown').is(":visible")) {
+        $('.dropdown-content').hide();
+    }
 })
