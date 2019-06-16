1609592abd1ee149d8eb12d3a689140734455ce2
Becki Blumer <rblumer13@gmail.com>
2019-06-16 10:49:31 -0500

    added some console.log fun

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ function toggleAbout() {
     $('.photo-fade').toggleClass('faded');
 };
 
+$('.js-about').one("click", function() {
+    console.log(`
+     /$$   /$$           /$$ /$$           /$$
+    | $$  | $$          | $$| $$          | $$
+    | $$  | $$  /$$$$$$ | $$| $$  /$$$$$$ | $$
+    | $$$$$$$$ /$$__  $$| $$| $$ /$$__  $$| $$
+    | $$__  $$| $$$$$$$$| $$| $$| $$  \ $$|__/
+    | $$  | $$| $$_____/| $$| $$| $$  | $$    
+    | $$  | $$|  $$$$$$$| $$| $$|  $$$$$$/ /$$
+    |__/  |__/ \_______/|__/|__/ \______/ |__/
+    Thank you for stopping by!
+    My favorite fact that didn't make it into my bio: I love to play the cello!`
+    )
+})
+
 $('.js-about').click(function() {
     toggleAbout();
 })
@@ -13,6 +28,10 @@ $('.learn-more').click(function() {
 
 $('.dropdown').click(function(){
     $('.dropdown-content').css("display", "block");
+})
+
+$('.meetthemusic').one("click", function() {
+    console.log(`This project was fun! I love going to see live music, especially in smaller venues with lesser-known artists. I wanted to make a tool I could use for myself, but also to share, to find the perfect concerts locally or while I'm out on a trip.`)
 })
 
 $(document).click(function(event) {
