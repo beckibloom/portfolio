5e492f90fdf0e7c71fc31e77ef1436eb4a7a9550
Becki Blumer <rblumer13@gmail.com>
2019-12-18 22:13:15 -0600

    polished responsive styles, added Ducklings and Activities projects

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-function toggleAbout() {
-    $('.image-text').toggleClass('hidden');
-    $('.photo-fade').toggleClass('faded');
-};
-
 $('.js-about').one("click", function() {
     console.log(`
      /$$   /$$           /$$ /$$           /$$
@@ -18,20 +13,24 @@ $('.js-about').one("click", function() {
     )
 })
 
-$('.js-about').click(function() {
-    toggleAbout();
-})
-
-$('.learn-more').click(function() {
-    toggleAbout();
-})
-
 $('.dropdown').click(function(){
     $('.dropdown-content').css("display", "block");
 })
 
 $('.meetthemusic').one("click", function() {
-    console.log(`This project was fun! I love going to see live music, especially in smaller venues with lesser-known artists. I wanted to make a tool I could use for myself, but also to share, to find the perfect concerts locally or while I'm out on a trip.`)
+    console.log(`Meet the Music was a fun project to build! I love going to see live music, especially in smaller venues with lesser-known artists. I wanted to make a tool I could use for myself, but also to share, to find the perfect concerts locally or while I'm out on a trip.`)
+})
+
+$('.ducklings').one("click", function() {
+    console.log(`Ducklings Care Diary was inspired by a teacher at my previous job who needed a better tool to communicate with her students' families. It was really gratifying to know that I was making something that would actually be really useful to someone!`)
+})
+
+$('.activitiesassistant').one("click", function() {
+    console.log(`Activities Assistant was inspired by my work at an elementary school where I was the coordinator for 100+ extracurricular activities. I had the responsibility to collect information on all the activities and advertise them effectively to the parents in our community, and I wished that I had a better way to collect and display all these details for parents - so I made one.`)
+})
+
+$('.chicago').one("click", function() {
+    console.log(`I absolutely adore the city of Chicago. I love public transit, so one of my favorite parts of living in Chicago is that the CTA can take me pretty much anywhere I need to go.`)
 })
 
 $(document).click(function(event) {
@@ -41,23 +40,3 @@ $(document).click(function(event) {
         $('.dropdown-content').hide();
     }
 })
-
-// function contactAnimation() {
-//     let element = document.getElementByClass("footer");
-//     element.addEventListener("click", function(e) {
-//         e.preventDefault;
-//         element.classList.remove("animate");
-//         void element.offsetWidth;
-//         element.classList.add("animate");
-//     }, false);
-    
-// }
-
-// contactAnimation();
-
-// $('.js-contact').click(function(event) {
-//      $('.footer').css("-webkit-animation", "yellow-fade 1s ease-in-out 0s");
-//     $('.footer').css("-moz-animation", "yellow-fade 1s ease-in-out 0s");
-//     $('.footer').css("-o-animation", "yellow-fade 1s ease-in-out 0s");
-//     $('.footer').css("animation", "yellow-fade 1s ease-in-out 0s");
-// })
